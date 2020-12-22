02ad67b573d43814bc190f2f0516460a34258ec2
Faruk <a.farukmollaoglu@gmail.com>
2020-12-22 23:05:01 +0300

    wipe

--- app/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  endOfLine: 'auto',
-  arrowParens: 'avoid',
-  semi: false,
-};
